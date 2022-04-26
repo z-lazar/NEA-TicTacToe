3ddf62fcbe6ede11ec8aaf275eed0145a92610fd
z-lazar <104430037+z-lazar@users.noreply.github.com>
2022-04-26 11:43:25 +0100

    Add files via upload

---
+++ TicTacToe.py
@@ -0,0 +1,12 @@
+from Ui import Gui, Terminal
+from sys import argv
+
+def usage():   
+    print(f"""
+Usage: {argv[0]} [g | t]
+g : play with the GUI
+t : play with the Terminal""")
+    quit()
+
+if __name__ == "__main__":
+    pass
---
+++ Ui.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+class Ui(ABC):
+
+    @abstractmethod
+    def run(self):
+        raise NotImplementedError
+
+class Gui(Ui):
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+class Terminal(Ui):
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
