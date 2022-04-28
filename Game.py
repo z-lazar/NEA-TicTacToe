7db9a11213d5d578bae63ebe25244497df8ce107
z-lazar <104430037+z-lazar@users.noreply.github.com>
2022-04-28 10:04:51 +0100

    Add files via upload

---
+++ Game.py
@@ -0,0 +1,16 @@
+class Game:
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def play(self, row, col):
+        pass
+
+    @property
+    def winner(self):
+        pass
+
+if __name__ == "__main__":
+    pass
